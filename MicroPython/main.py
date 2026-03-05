691b8fb79ca9c434d68c2b8da831c080ab1ff87d
Ashlyn-Anpalahan <ashlyn.anpalahan@stu.ocsb.ca>
2026-03-05 22:56:30 +0000

    Python RBG Code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,69 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Ashlyn
+Created on: Feb 2026
+This module will allow an RGB LED to scroll through different colors
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+# setup
+display.clear()
+display.show(Image.HAPPY)
+
+# Button A
+while True:
+    if button_a.is_pressed():
+        # Red
+        pin13.write_digital(1)
+        pin14.write_digital(0)
+        pin15.write_digital(0)
+        display.scroll("Red")
+        sleep(1000)
+
+        # Blue
+        pin13.write_digital(0)
+        pin14.write_digital(0)
+        pin15.write_digital(1)
+        display.scroll("Blue")
+        sleep(1000)
+
+        # Green
+        pin13.write_digital(0)
+        pin14.write_digital(1)
+        pin15.write_digital(0)
+        display.scroll("Green")
+        sleep(1000)
+
+        # Magenta
+        pin13.write_digital(1)
+        pin14.write_digital(0)
+        pin15.write_digital(1)
+        display.scroll("Magenta")
+        sleep(1000)
+
+        # Yellow
+        pin13.write_digital(1)
+        pin14.write_digital(1)
+        pin15.write_digital(0)
+        display.scroll("Yellow")
+        sleep(1000)
+
+        # Cyan
+        pin13.write_digital(0)
+        pin14.write_digital(1)
+        pin15.write_digital(1)
+        display.scroll("Cyan")
+        sleep(1000)
+
+        # White
+        pin13.write_digital(1)
+        pin14.write_digital(1)
+        pin15.write_digital(1)
+        display.scroll("White")
+        sleep(1000)
+
+        # Turn Off
+        pin13.write_digital(0)
+        pin14.write_digital(0)
+        pin15.write_digital(0)
+        sleep(1000)
